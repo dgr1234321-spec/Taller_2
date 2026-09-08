0a774f1c72b9c32304316a38191adfaa92126e2a
dgr1234321-spec <dgr1234321@gmail.com>
2026-09-07 19:35:35 -0500

    Update PerfilEstudianteUTP.java

--- PerfilEstudianteUTP.java
+++ PerfilEstudianteUTP.java
@@ -43,7 +43,7 @@ public class PerfilEstudianteUTP {
         
         // TODO 4: Utiliza tus variables y constantes corregidas para calcular subtotal y total
         double subtotalMatricula = creditosmatriculados * costoCredito;
-        double totalPagar + = subtotalMatricula;
+        double totalPagar + = subtotalMatricula + CARNET_ESTUDIANTIL;
 
         System.out.println("==========================================");
         System.out.println("    FICHA DE MATRÍCULA ESTUDIANTIL UTP");
